98cf48a067cddb320d6db59ae43567702f9e2cc5
Tim Gremore <tim@headway.io>
2023-11-29 09:38:16 -0600

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,47 @@
+defmodule KinoAmazonKeywords.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @description "Create dataframe from popular search terms from Amazon"
+
+  def project do
+    [
+      app: :kino_amazon_keywords,
+      description: @description,
+      version: @version,
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:kino, "~> 0.11.3"},
+      {:req, "~> 0.4.0"},
+      {:explorer, "~> 0.7.1"},
+      {:kino, "~> 0.11.0"},
+      {:kino_explorer, "~> 0.1.11"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "components",
+      source_url: "https://github.com/headwayio/kino_amazon_keywords",
+      source_ref: "v#{@version}",
+      extras: []
+    ]
+  end
+end
---
+++ test/kino_amazon_keywords_test.exs
@@ -0,0 +1,8 @@
+defmodule KinoAmazonKeywordsTest do
+  use ExUnit.Case
+  doctest KinoAmazonKeywords
+
+  test "greets the world" do
+    assert KinoAmazonKeywords.hello() == :world
+  end
+end
