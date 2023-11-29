740072a58c7fd51aaa82bd0792fda71403516f06
Tim Gremore <tim@headway.io>
2023-11-29 09:41:50 -0600

    feat: Register smart cell

---
+++ lib/application.ex
@@ -0,0 +1,14 @@
+defmodule KinoAmazonKeywords.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    Kino.SmartCell.register(KinoAmazonKeywords)
+
+    children = []
+    opts = [strategy: :one_for_one, name: KinoAmazonKeywords.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
