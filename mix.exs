bcaaa8f1a465ef2bf9ef5235dc67cb930374aced
Tim Gremore <tim@headway.io>
2023-12-06 08:58:33 -0600

    chore: Version bump 0.1.5

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule KinoAmazonKeywords.MixProject do
   use Mix.Project
 
-  @version "0.1.4"
+  @version "0.1.5"
   @description "Create dataframe from popular search terms from Amazon"
 
   def project do
