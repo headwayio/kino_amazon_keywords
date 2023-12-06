e2a494fe3a10e24fe972fab6cf95ea3b69246947
Tim Gremore <tim@headway.io>
2023-12-06 08:33:13 -0600

    fix: Encode query params

--- lib/kino_amazon_keywords/keywords.ex
+++ lib/kino_amazon_keywords/keywords.ex
@@ -20,6 +20,7 @@ defmodule KinoAmazonKeywords.Keywords do
 
     url =
       "https://completion.amazon.com/api/2017/suggestions?limit=11&prefix=#{prefix}&suggestion-type=#{suggestion_type1}&suggestion-type=#{suggestion_type2}&page-type=Search&alias=aps&site-variant=desktop&version=3&event=onkeypress&wc=&lop=en_US&last-prefix=#{last_prefix}&avg-ks-time=567&fb=1&session-id=147-2944963-3693218&request-id=9TT8PTZBJASW7WDSG87R&mid=ATVPDKIKX0DER&plain-mid=1&client-info=amazon-search-ui"
+      |> URI.encode()
 
     {:ok, response} = Req.get(url)
 
